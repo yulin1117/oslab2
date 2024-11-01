980622a3cd91026e9c578a58ecca5743ecc76378
yulin1117 <123855859+yulin1117@users.noreply.github.com>
2024-11-01 13:17:52 +0800

    Update thread_local_variable.c

--- thread_local_variable.c
+++ thread_local_variable.c
@@ -1,3 +1,8 @@
+/* 
+1.use "__thread" to create a variable only can be save or modify by particular thread
+2.if u want to send a value into pthread_create(), use poiter
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -17,7 +22,7 @@ static void *incr(void *loop){
 int main(){
 	int p1 = 50, p2 = 100;
 	//before
-	printf("(main thread) glob value after run 2 threads %d\n", glob);
+	printf("(main thread) glob value after run 2 threads %d\n", glob); //should equal to 0 bc child thread cannot modify the value of the variable in main thread
 
 	pthread_t id[2];
 	pthread_create(&id[0],NULL,incr,&p1);
